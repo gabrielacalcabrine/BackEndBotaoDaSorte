89958f7af74d7ed46fab2b02f9dcc1568dd99d8e
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-11-26 21:26:38 -0300

    criando BackEnd botao

---
+++ NumeroDaSorte/Controllers/NumeroDaSorte.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NumeroDaSorte.Controllers
+{
+    [ApiController]
+    [Route("NumeroSorte")]
+    public class NumeroDaSorte : ControllerBase
+    {        
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        public async Task<ActionResult<int>> GetNumeroDaSorteAsync()
+        {
+            Random random = new Random();
+            var numeroAleatorio = random.Next(1, 101);
+            return Ok(numeroAleatorio);
+        }      
+    }
+}
